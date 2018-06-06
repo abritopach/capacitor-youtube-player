58a9a477d0879c1a2d07512fc6518450d42a12a9
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-06-06 16:51:03 +0200

    Updated ios

--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -5,8 +5,8 @@ import Capacitor
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
-@objc(YoutubePlayerPlugin)
-public class YoutubePlayerPlugin: CAPPlugin {
+@objc(YoutubePlayer)
+public class YoutubePlayer: CAPPlugin {
     
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
