f18abd2e2e779f7186f448ce6436b519c15a5101
abritopach <adrbrpa1988@gmail.com>
2019-11-05 21:51:41 +0100

    Updated readme

--- src/definitions.ts
+++ src/definitions.ts
@@ -9,15 +9,15 @@ declare global {
 export interface YoutubePlayerPlugin {
   echo(options: { value: string }): Promise<{value: string}>;
   initialize(options: {width: number, height: number, videoId: string, playerVars?: IPlayerVars}): Promise<{playerReady: boolean}>;
-  destroy(): Promise<{result: { method: string, value: boolean }}>;
+  destroy(playerId: string): Promise<{result: { method: string, value: boolean }}>;
   // Methods playback controls and player settings..
   /***********/
-  stopVideo(): Promise<{result: { method: string, value: boolean }}>;
-  playVideo(): Promise<{result: { method: string, value: boolean }}>;
-  pauseVideo(): Promise<{result: { method: string, value: boolean }}>;
-  seekTo(): Promise<{result: { method: string, value: boolean, seconds: number, allowSeekAhead: boolean }}>;
-  loadVideoById(options: {videoId: string, startSeconds?: number, endSeconds?: number, suggestedQuality?: string}): Promise<{result: { method: string, value: boolean, options: {} }}>;
-  cueVideoById(options: {videoId: string, startSeconds?: number, endSeconds?: number, suggestedQuality?: string}): Promise<{result: { method: string, value: boolean, options: {} }}>;
+  stopVideo(playerId: string): Promise<{result: { method: string, value: boolean }}>;
+  playVideo(playerId: string): Promise<{result: { method: string, value: boolean }}>;
+  pauseVideo(playerId: string): Promise<{result: { method: string, value: boolean }}>;
+  seekTo(playerId: string, seconds: number, allowSeekAhead: boolean): Promise<{result: { method: string, value: boolean, seconds: number, allowSeekAhead: boolean }}>;
+  loadVideoById(playerId: string, options: {videoId: string, startSeconds?: number, endSeconds?: number, suggestedQuality?: string}): Promise<{result: { method: string, value: boolean, options: {} }}>;
+  cueVideoById(playerId: string, options: {videoId: string, startSeconds?: number, endSeconds?: number, suggestedQuality?: string}): Promise<{result: { method: string, value: boolean, options: {} }}>;
   /***********/
 
   // Methods changing the player volume.
