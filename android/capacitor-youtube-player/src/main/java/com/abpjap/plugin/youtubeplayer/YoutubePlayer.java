1754445402c7bbe9c1d7a2b9692675342eb17a4e
abritopach <adrbrpa1988@gmail.com>
2021-01-17 23:29:27 +0000

    Used the new @CapacitorPlugin annotation

--- android/capacitor-youtube-player/src/main/java/com/abpjap/plugin/youtubeplayer/YoutubePlayer.java
+++ android/capacitor-youtube-player/src/main/java/com/abpjap/plugin/youtubeplayer/YoutubePlayer.java
@@ -5,16 +5,16 @@ import android.content.Intent;
 import android.util.Log;
 
 import com.getcapacitor.JSObject;
-import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.android.youtube.player.YouTubePlayer;
 
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
-@NativePlugin()
+@CapacitorPlugin()
 public class YoutubePlayer extends Plugin {
 
     private static final String TAG = YouTubePlayer.class.getSimpleName();
@@ -36,7 +36,7 @@ public class YoutubePlayer extends Plugin {
 
         JSObject ret = new JSObject();
         ret.put("value", value);
-        call.success(ret);
+        call.resolve(ret);
     }
 
     @PluginMethod()
@@ -65,7 +65,7 @@ public class YoutubePlayer extends Plugin {
 
                     JSObject ret = new JSObject();
                     ret.put("value", message);
-                    call.success(ret);
+                    call.resolve(ret);
                 }
             }
         });
