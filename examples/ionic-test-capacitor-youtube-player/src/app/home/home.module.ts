82de901eafc946bd102a1d617e7a66b5491e74c7
abritopach <adrbrpa1988@gmail.com>
2021-11-21 20:58:36 +0000

    ⚡ Updated ionic test capacitor youtube player

--- examples/ionic-test-capacitor-youtube-player/src/app/home/home.module.ts
+++ examples/ionic-test-capacitor-youtube-player/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NtkmeButtonModule
   ],
   declarations: [HomePage]
 })
--- examples/ionic-test-capacitor-youtube-player/src/app/home/home.page.ts
+++ examples/ionic-test-capacitor-youtube-player/src/app/home/home.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+import { YoutubePlayer } from 'capacitor-youtube-player';
+
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, AfterViewInit {
 
-  constructor() {}
+  currentYear = new Date().getFullYear();
+
+  constructor() {
+    console.log('HomePage::constructor() | method called');
+  }
+
+  ngOnInit() {
+    console.log('HomePage::ngOnInit() | method called');
+  }
+
+  ngAfterViewInit() {
+    if (Capacitor.getPlatform() === 'web') {
+      this.initializeYoutubePlayerPluginWeb();
+    } else { // Native
+      this.initializeYoutubePlayerPluginNative();
+    }
+  }
+
+  async initializeYoutubePlayerPluginWeb() {
+    console.log('HomePage::initializeYoutubePlayerPluginWeb() | method called');
+    const options = {playerId: 'youtube-player', playerSize: {width: 640, height: 360}, videoId: 'tDW2C6rcH6M', debug: true};
+    const result = await YoutubePlayer.initialize(options);
+    console.log('playerReady', result);
+
+    (result as any).player.addEventListener('onPlaybackQualityChange', (event) => {
+      console.log('playback quality is', event);
+    });
+
+    (result as any).player.addEventListener('onStateChange', (event) => {
+      console.log('state is', event);
+    });
+
+    const options1 = {playerId: 'youtube-player1', playerSize: {width: 640, height: 360}, videoId: 'M1F81V-NhP0'};
+    const result1 = await YoutubePlayer.initialize(options1);
+    console.log('playerReady', result1);
+  }
+
+  async destroyYoutubePlayerPluginWeb() {
+    console.log('HomePage::destroyYoutubePlayerPluginWeb() | method called');
+    const result = await YoutubePlayer.destroy('youtube-player');
+    console.log('destroyYoutubePlayer', result);
+  }
+
+  async getPlayersEventsStatePluginWeb() {
+    console.log('HomePage::getPlayersEventsStatePluginWeb() | method called');
+    const result = await YoutubePlayer.getAllPlayersEventsState();
+    console.log('allPlayersEventsState', result);
+  }
+
+  async initializeYoutubePlayerPluginNative() {
+
+    const options = {playerSize: {width: 640, height: 360}, videoId: 'tDW2C6rcH6M'};
+    const playerReady = await YoutubePlayer.initialize(options);
+  }
 
 }
