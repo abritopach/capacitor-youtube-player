4ffea71f4e4fe98a225041224c1b0cfbdb9c5aee
abritopach <adrbrpa1988@gmail.com>
2022-02-23 19:36:30 +0000

    📦 Updated @angular dependencies in package.json

--- examples/ionic-test-capacitor-youtube-player/src/app/app.module.ts
+++ examples/ionic-test-capacitor-youtube-player/src/app/app.module.ts
@@ -8,10 +8,9 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
